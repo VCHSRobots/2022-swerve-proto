e2be32d7e5b52342f205fade20b1d8a87c6dfc1a
AKcreepy1 <44420919+AKcreepy1@users.noreply.github.com>
2022-01-18 19:55:55 -0800

    Added USB Camera Compatibility

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -4,6 +4,7 @@
 
 package frc.robot;
 
+import edu.wpi.first.cameraserver.CameraServer;;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
@@ -52,6 +53,8 @@ public class Robot extends TimedRobot {
 
     autonomous = new Auto();
     autonomous.robotInit();
+
+    CameraServer.startAutomaticCapture();
 
   }
 
