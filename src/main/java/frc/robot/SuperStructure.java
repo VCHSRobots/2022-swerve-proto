467c9832b2dec8663e43769ae60fdfac17846ad8
IAN <73033682+IAN123456789012345678901234567890@users.noreply.github.com>
2022-02-01 19:12:14 -0800

    Update SuperStructure with else
    Do not run else statement

--- src/main/java/frc/robot/SuperStructure.java
+++ src/main/java/frc/robot/SuperStructure.java
@@ -3,6 +3,7 @@
 // the WPILib BSD license file in the root directory of this project.
 
 package frc.robot;
+
 import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
 import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
 import edu.wpi.first.networktables.NetworkTableEntry;
@@ -15,11 +16,11 @@ public class SuperStructure extends Base {
     private Shooter m_Shooter;
     private ColorSensor m_ColorSensor;
     private Climber m_Climber;
-    ShuffleboardTab ShootMotor1Tab = Shuffleboard.getTab("Shooter1");
 
-    NetworkTableEntry ntBotRPM = ShootMotor1Tab.add("Bot RPM", 1000).withPosition(3, 3).withSize(1, 1).getEntry();
-    NetworkTableEntry ntTopRPM = ShootMotor1Tab.add("Top RPM", 1000).withPosition(3, 2).withSize(1, 1).getEntry();
-    NetworkTableEntry ntFeetToRPM = ShootMotor1Tab.add("Feet To Top RPM", 17).withPosition(4, 2).withSize(1, 1)
+    ShuffleboardTab ShootMotorTab = Shuffleboard.getTab("ShooterSuper");
+    NetworkTableEntry ntBotRPM = ShootMotorTab.add("Bot RPM", 1000).withPosition(3, 3).withSize(1, 1).getEntry();
+    NetworkTableEntry ntTopRPM = ShootMotorTab.add("Top RPM", 1000).withPosition(3, 2).withSize(1, 1).getEntry();
+    NetworkTableEntry ntFeetToRPM = ShootMotorTab.add("Feet To Top RPM", 17).withPosition(4, 2).withSize(1, 1)
             .getEntry();
 
     public SuperStructure(SwerveDrive swerveDrive, Intake intake, Shooter shooter, ColorSensor colorSensor,
@@ -39,7 +40,6 @@ public class SuperStructure extends Base {
         m_Intake.init();
         m_Shooter.robotInit();
         m_ColorSensor.init();
-
         m_Climber.init();
 
     }
@@ -62,14 +62,13 @@ public class SuperStructure extends Base {
         m_Shooter.TurnTable(OI.getRightBumperForTurntable(), OI.getLeftBumperForTurntable());
         m_Climber.climberMove(OI.getSolenoidToggle(), OI.getArmsUp(), OI.getArmsDown());
 
-        
         if (OI.getYButtonForShootRPM()) {
             // turn shooter on in rpm mode
             m_Shooter.shootingRPM(ntTopRPM.getNumber(0).doubleValue(), ntBotRPM.getNumber(0).doubleValue());
-            
 
             if (m_Shooter.IsOkToShoot()) {
                 // Load shooter
+                m_Intake.loadShooter();
 
             }
         } else if (OI.getXButtonForToggleFeetToDist()) {
@@ -78,10 +77,11 @@ public class SuperStructure extends Base {
 
             if (m_Shooter.IsOkToShoot()) {
                 // load shooter
+                m_Intake.loadShooter();
             }
-
         } else {
-
+            m_Shooter.shootingRPM(0, 0);
+            m_Shooter.shootingDist(0);
         }
 
     }
@@ -99,5 +99,6 @@ public class SuperStructure extends Base {
         m_SwerveDrive.test(OI.xboxDrive.getAButton());
 
     }
+    
 
 }
