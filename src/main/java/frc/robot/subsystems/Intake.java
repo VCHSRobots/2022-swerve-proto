46c08656a70e538be09c6bf369939f8226bce8f2
ABCpduck27 <90739666+ABCpduck27@users.noreply.github.com>
2022-02-04 19:32:19 -0800

    moved intake to folder

--- src/main/java/frc/robot/subsystems/Intake.java
+++ src/main/java/frc/robot/subsystems/Intake.java
@@ -2,7 +2,7 @@
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
 
-package frc.robot;
+package frc.robot.subsystems;
 
 import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.NeutralMode;
@@ -16,9 +16,8 @@ import edu.wpi.first.wpilibj.PneumaticsModuleType;
 import edu.wpi.first.wpilibj.DoubleSolenoid.Value;
 import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
 import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
+import frc.robot.RobotMap;
 import edu.wpi.first.wpilibj.Timer;
-import frc.robot.subsystems.Base;
-import frc.robot.subsystems.ColorSensor;
 
 /** Add your docs here. */
 public class Intake extends Base {
