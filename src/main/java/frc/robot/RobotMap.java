ccc0a788eef27e11da12ef5e5586763993ad7807
IAN <73033682+IAN123456789012345678901234567890@users.noreply.github.com>
2022-02-26 16:19:17 -0800

    Update RobotMap.java

--- src/main/java/frc/robot/RobotMap.java
+++ src/main/java/frc/robot/RobotMap.java
@@ -27,22 +27,14 @@ public class RobotMap {
     public static final int kDrive_BackLeftTurn_TalonFX     = 5;
     public static final int kDrive_BackRightDrive_TalonFX   = 8;
     public static final int kDrive_BackRightTurn_TalonFX    = 7;
-    
-    // public static final int kDrive_BackRightDrive_TalonFX   = 8;
-    // public static final int kDrive_BackRightTurn_TalonFX    = 7;
-    // public static final int kDrive_BackLeftDrive_TalonFX  = 2;
-    // public static final int kDrive_BackLeftTurn_TalonFX   = 1;
-    // public static final int kDrive_FrontRightDrive_TalonFX    = 6;
-    // public static final int kDrive_FrontRightTurn_TalonFX     = 5;
-    // public static final int kDrive_FrontLeftDrive_TalonFX   = 8;
-    // public static final int kDrive_FrontLeftTurn_TalonFX    = 7;
 
     public static final int kIntake_roller_TalonFX          = 9;
     public static final int kIntake_mover_TalonFX           = 10;
     public static final int kIntake_shooterInput_TalonFX    = 15;
 
     public static final int kClimb_master_TalonFX          = 21;
-    public static final int kClimb_follower_TalonFX         = 22;
+    public static final int kClimb_follower_TalonFX        = 22;
+    public static final int kClimb_follower2_TalonFX       = 23;
     
     public static final int kShoot_BottomMotor_TalonFX     = 31;
     public static final int kShoot_TopMotor_TalonFX        = 32;
@@ -54,10 +46,6 @@ public class RobotMap {
     public static final int kDrive_FrontRightEncoder        = 11;
     public static final int kDrive_BackLeftEncoder          = 13;
     public static final int kDrive_BackRightEncoder         = 14;
-    // public static final int kDrive_BackRightEncoder         = 12;
-    // public static final int kDrive_BackLeftEncoder        = 11;
-    // public static final int kDrive_FrontRightEncoder          = 13;
-    // public static final int kDrive_FrontLeftEncoder         = 14;
     // END CANCODER //
 
     // SOLENOID //
