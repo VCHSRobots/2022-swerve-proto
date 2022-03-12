4dcbc6137b554d3ce951139e903c181c7e1873ab
Todd <toddzakk@gmail.com>
2022-03-12 08:09:47 -0800

    updated vision shooter code

---
+++ src/main/java/frc/robot/cancoder-readme.txt
@@ -0,0 +1,17 @@
+counterclock-wise is increasing for cancoder
+
+minimum raw: -17, intake direction, a little toward the light.
+maximum: 445, toward the light.
+total range (462)
+
+boot up raw cancoder value: -180 to 180
+offset: -123
+our boot value range:-303 to 57. then add 360 if less than our range.
+
+our range is : -135 to 300
+
+raw+offset=our value. our zero is point at the intake. 
+if boot up is -180 to 180, then any time range: (-180, 180+462)
+
+
+
--- src/main/java/frc/robot/subsystems/VisionShooter.java
+++ src/main/java/frc/robot/subsystems/VisionShooter.java
@@ -4,8 +4,6 @@
 
 package frc.robot.subsystems;
 
-import org.photonvision.targeting.PhotonTrackedTarget;
-
 import edu.wpi.first.math.util.Units;
 import edu.wpi.first.networktables.NetworkTable;
 import edu.wpi.first.networktables.NetworkTableEntry;
@@ -13,23 +11,9 @@ import edu.wpi.first.networktables.NetworkTableInstance;
 import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
 import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
 
-/**
- * Find a blue or red ball and drive toward it.
- * for now, only work in teleop mode.
- * 1. Read the most likely target detected by the PhotonVision RPi device
- * from the Network table and display the target on shuffleboard
- * 2. drive toward it.
- * - what if the ball is moving
- * 3. if no ball found, turn around to find the ball.
- */
 public class VisionShooter extends Base {
 
     ShuffleboardTab visionBalltab = Shuffleboard.getTab("VisionShooter");
-
-    // rename these in network tables if camera is rotated
-    // NetworkTableEntry ntTargetYaw = visionBalltab.add("Yaw", 0).getEntry();
-    // NetworkTableEntry ntTargetPitch = visionBalltab.add("Pitch", 0).getEntry();
-    // NetworkTableEntry ntTargetArea = visionBalltab.add("Area", 0).getEntry();
 
     NetworkTable table = NetworkTableInstance.getDefault().getTable("limelight");
     NetworkTableEntry ntTargetYaw = table.getEntry("tx");
@@ -37,57 +21,21 @@ public class VisionShooter extends Base {
     NetworkTableEntry ntTargetArea = table.getEntry("ta");
     NetworkTableEntry ntTargetValid = table.getEntry("tv");
 
-    private PhotonTrackedTarget m_lastTarget;
     private double m_lastTx = 0;
     private double m_lastTy = 0;
     private boolean hasLastTarget = false;
     // private PhotonCamera camera = new PhotonCamera("mmal_service_16.1");
     private int m_lostCount = 0;
     private int m_icount = 0;
-    private int m_MAXLOSTCOUNT = 10;
-    private double m_yawOffset = 0;
-
-    // public VisionBall(SwerveDrive sd) {
-    public VisionShooter() {
-        // m_SwerveDrive=sd;
-        // connectCamera();
-    }
+    private int m_MAXLOSTCOUNT = 7;
+    private double m_max_angle_change_in_20ms=10.0;
 
     @Override
     public void robotInit() {
-        // var cameraName="Microsoft_LifeCam_HD-3000";
-        // connectCamera();
         Shuffleboard.getTab("shooter debug").addNumber("Vision Yaw", ()-> getYaw());
     }
 
-    public void connectCamera() {
-        var cameraName = "mmal_service_16.1";
-        // if (camera == null)
-        //     camera = new PhotonCamera(cameraName);
-    }
-
     public double calculateAngleError() {
-        // connectCamera();
-        // if (camera == null)
-        //     return -1;
-        // var result = camera.getLatestResult();
-        // System.out.println("calc angle");
-
-        // update shuffle board
-        m_icount++;
-        if (m_icount % 50 == 0) {
-            m_icount = 0;
-            // if (result.hasTargets()) {
-            //     ntTargetYaw.setDouble(result.getBestTarget().getYaw());
-            //     ntTargetPitch.setDouble(result.getBestTarget().getPitch());
-            //     ntTargetArea.setDouble(result.getBestTarget().getArea());
-            // } else {
-            //     ntTargetYaw.setDouble(-1);
-            //     ntTargetPitch.setDouble(-1);
-            //     ntTargetArea.setDouble(-1);
-            // }
-        }
-
         /*
          * vision filter:
          * 1. prevent jumping between two different objects
@@ -102,25 +50,20 @@ public class VisionShooter extends Base {
         double newTx;
         double newTy;
         var hasTarget = false;
-        // if (!result.hasTargets()) {
-        if (!(ntTargetValid.getDouble(0) == 1)) {
+
+        if (ntTargetValid.getDouble(0) == 0) {
             if (hasLastTarget) {
-                // target = m_lastTarget;
                 newTx = m_lastTx;
                 newTy = m_lastTy;
             } else {
-                // m_SwerveDrive.drive(0, 0, 0, false);
-                // return false;
                 return 0;
             }
         } else {
             hasTarget = true;
-            // target = result.getBestTarget();
             newTx = ntTargetYaw.getDouble(0);
             newTy = ntTargetPitch.getDouble(0);
             if (hasLastTarget) {
-                if (Math.abs((newTx - m_lastTx) / m_lastTx) > 0.7) {
-                        // || Math.abs(target.getArea() - m_lastTarget.getArea()) / m_lastTarget.getArea() > 0.5) {
+                if (Math.abs((newTx - m_lastTx)) > m_max_angle_change_in_20ms) {
                     hasTarget = false;
                 }
             }
@@ -136,7 +79,6 @@ public class VisionShooter extends Base {
             m_lostCount = 0;
         }
 
-        // m_lastTarget = target;
         m_lastTx = newTx;
         m_lastTy = newTy;
         hasLastTarget = true;
@@ -144,25 +86,21 @@ public class VisionShooter extends Base {
     }
 
     public double getYaw() {
-        // if (m_lastTarget == null)
-        //     return 0.0;
         return -ntTargetYaw.getDouble(0);
-        // return -m_lastTx;
     }
 
     public double getPitch() {
-        // change this is camera is rotated
-        // if (m_lastTarget == null)
-        //     return 15.0;
         return m_lastTy;
     }
 
     public double getDistance() {
         double visionTargetOffsetFromCenter = 2;
-        double distanceFeet = 14;
-        double cameraAngle = Units.degreesToRadians(25.0); // angle from horizontal to axis of camera view
+        double distanceFeet = 0;
+        double cameraAngle = 60.0; // angle from horizontal to axis of camera view, in degress
         double cameraHeight = 2; // height of camera on robot
         double targetHeight = 8.46833; // height of retroreflective tape in feet
+
+        double pitch=getPitch();
         /*
          * from internet
          * distance = delta_height / (tan(cameraAngle + Pitch) * cos(Yaw))
@@ -170,7 +108,7 @@ public class VisionShooter extends Base {
          * d = (targetHeight - cameraHeight) / tan(cameraAngle + Pitch)
          */
         distanceFeet = visionTargetOffsetFromCenter + (targetHeight - cameraHeight)
-                / (Math.tan(cameraAngle + Units.degreesToRadians(getPitch())) * Math.cos(getYaw()));
+                / Math.tan(Units.degreesToRadians(cameraAngle + pitch));
         return distanceFeet;
     }
 
